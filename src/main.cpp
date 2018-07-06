f89f09190b93a940bbbe8506744571146b2a389b
EC2 Default User <ec2-user@ip-172-31-24-175.eu-west-1.compute.internal>
2018-07-06 15:35:22 +0000

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -11,31 +11,25 @@
 // https://github.com/cmmakerclub/esp32-webserver
 
 #undef EPS
-#include <unistd.h> // add to gzguts.h
-//#include "opencv2/calib3d.hpp"
-//#include "opencv2/imgproc.hpp"
-#include "opencv2/core.hpp"
+//#include "opencv2/core/calib3d.hpp"
+//#include "opencv2/core/imgproc.hpp"
+#include "opencv2/core/core.hpp"
 
 //#include "opencv2/objdetect.hpp"
 //#include "opencv2/highgui/highgui.hpp"
 //#include "opencv2/imgproc.hpp"
 #define EPS		192
-
+ 
 #include "OV7670.h"
 //#include <WiFi.h>
 //#include <WiFiMulti.h>
 //#include <WiFiClient.h>
 #include "BMP.h"
 
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "sdkconfig.h"
 
-using namespace std;
 using namespace cv;
-
-cv::Mat* img1;
 
 const int SIOD = 22; //SDA
 const int SIOC = 23; //SCL
@@ -66,30 +60,19 @@ const int D7 = 17;
 //#define password2    ""
 
 OV7670 *camera;
-
-//WiFiMulti wifiMulti;
-//WiFiServer server(80);
-
 unsigned char bmpHeader[BMP::headerSize];
-
-// Function Headers
-//void detectAndDisplay(cv::Mat frame);
-
-// Global variables
-// Copy this file from opencv/data/haarscascades to target folder
-std::string face_cascade_name = "c:/haarcascade_frontalface_alt.xml";
-//cv::CascadeClassifier face_cascade;
-std::string window_name = "Capture - Face detection";
-int filenumber; // Number of file to be saved
-std::string filename;
 
 static bool endsWith(const std::string& str, const std::string& suffix)
 {
     return str.size() >= suffix.size() && 0 == str.compare(str.size()-suffix.size(), suffix.size(), suffix);
 }
 
+/*
+WiFiMulti wifiMulti;
+WiFiServer server(80);
+
 void serve()
-{/*
+{
   WiFiClient client = server.available();
   if (client) 
   {
@@ -101,7 +84,6 @@ void serve()
       if (client.available()) 
       {
         char c = client.read();
-        //Serial.write(c);
         if (c == '\n') 
         {
           if (currentLine.length() == 0) 
@@ -132,10 +114,11 @@ void serve()
             client.println("Content-type:image/bmp");
             client.println();
             
-            for(int i = 0; i < BMP::headerSize; i++)
-               client.write(bmpHeader[i]);
+            //for(int i = 0; i < BMP::headerSize; i++)
+            //   client.write(bmpHeader[i]);
             //for(int i = 0; i < camera->xres * camera->yres * 2; i++)
             //   client.write(camera->frame[i]);
+            client.write((const uint8_t*)(bmpHeader), (size_t)(BMP::headerSize));
             client.write((const uint8_t*)(camera->frame), (size_t)(camera->xres * camera->yres * 2));
             
         }
@@ -144,14 +127,15 @@ void serve()
     // close the connection:
     client.stop();
     //Serial.println("Client Disconnected.");
-  }  */
+  }  
 }
+*/
 
 void loop(void *pvParameter)
+//void loop()
 {
   camera->oneFrame();
   //serve();
-  //displayRGB565(camera->frame, camera->xres, camera->yres);
 }
 
 extern "C" {
@@ -159,10 +143,12 @@ extern "C" {
 }
 
 void app_main()
+//void setup()
 {
-  //Mat mat(160, 120, cv::DataType<int>::type);
-//  Serial.begin(115200);
-/*
+  Mat mat(160, 120, cv::DataType<int>::type);
+/*  
+  Serial.begin(115200);
+
   wifiMulti.addAP(ssid1, password1);
   //wifiMulti.addAP(ssid2, password2);
   Serial.println("Connecting Wifi...");
@@ -172,7 +158,7 @@ void app_main()
       Serial.println("IP address: ");
       Serial.println(WiFi.localIP());
   }
-  */
+*/  
   camera = new OV7670(OV7670::Mode::QQVGA_RGB565, SIOD, SIOC, VSYNC, HREF, XCLK, PCLK, D0, D1, D2, D3, D4, D5, D6, D7);
   BMP::construct16BitHeader(bmpHeader, camera->xres, camera->yres);
   
